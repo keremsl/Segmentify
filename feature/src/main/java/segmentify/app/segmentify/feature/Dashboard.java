66687bd2581fe94a68e33911c2c6970e04b7b0f6
keremsl <mehmetkeremceylan@gmail.com>
2018-04-18 16:46:15 +0300

    Initial commit

---
+++ feature/src/main/java/segmentify/app/segmentify/feature/Dashboard.java
@@ -0,0 +1,13 @@
+package segmentify.app.segmentify.feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.interaction);
+    }
+}
