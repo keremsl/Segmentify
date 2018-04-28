ffc8271b654214342c0f1a7d7b56a83f1ccf25c6
keremsl <mehmetkeremceylan@gmail.com>
2018-04-28 19:21:46 +0300

    added widget info dialog and dialog's lib && set title

--- feature/src/main/java/segmentify/app/segmentify/feature/MainActivity.java
+++ feature/src/main/java/segmentify/app/segmentify/feature/MainActivity.java
@@ -1,14 +1,38 @@
 package segmentify.app.segmentify.feature;
 
+import android.graphics.Color;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
-
+        ImageView widgetInfoImg = findViewById(R.id.btnWidgetInfo);
+        this.setTitle("Segmentify Performance");
+        this.setTitleColor(Color.CYAN);
+        widgetInfoImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialDialog.Builder(v.getContext())
+                        .title("Merhaba")
+                        .content("kerem")
+                        .positiveText("OKAY")
+                        .show();
             }
+        });
+
+
+    }
+
 }
